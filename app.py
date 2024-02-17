49bfc8c4c33f33f8ac2c9371e6b6e780564038c2
Anima Regem <vichukartha@gmail.com>
2024-02-17 12:16:34 +0530

    final

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import google.generativeai as genai
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from pymongo import MongoClient
+from flask_cors import CORS
 from functools import reduce
 
 
 
 load_dotenv()
 app = Flask(__name__)
+CORS(app)
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 print(GOOGLE_API_KEY)
 MONGO_URI = os.getenv('MONGODB_URI')
@@ -26,7 +28,7 @@ def quote():
 @app.post("/chat")
 def chat():
     request_body = request.json.get("message")
-    response = model.generate_content("You are CrumsAI, an AI Assistant that can help with food leftover managment, you can help in giving ideas for food management, food planning and leftover management\n"+request_body, stream=False)
+    response = model.generate_content("You are CrumsAI, an AI Assistant that can help with food leftover managment, you can help in giving ideas for food management, food planning and leftover management.\nRules:(1. Reply should be short and consise 2. Do not reutrn markdown of any kind of formatting)\n"+request_body, stream=False)
     return response.text
 
 @app.post("/get_insights")
